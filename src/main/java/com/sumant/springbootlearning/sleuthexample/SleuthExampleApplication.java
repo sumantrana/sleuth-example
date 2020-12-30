e56f3c0f1b60e3bac27ca3542a662cdea06c827d
Sumant Rana <sumantrana@rsumantsing-a01.vmware.com>
2020-12-30 17:46:57 +0800

    Initial Commit

---
+++ src/main/java/com/sumant/springbootlearning/sleuthexample/SleuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sumant.springbootlearning.sleuthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sumant/springbootlearning/sleuthexample/SleuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumant.springbootlearning.sleuthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
